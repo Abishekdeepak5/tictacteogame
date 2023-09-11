923649167fd4342fd4aac6ccfec188ecb044bb44
Abishekdeepak5 <143536892+Abishekdeepak5@users.noreply.github.com>
2023-09-11 18:11:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ var roomno = 1;
 var full = 0;
 
 io.on('connection', async (socket) => {
-  console.log('a user connected');
+  // console.log('a user connected');
   const name = socket.handshake.query.name;
   socket.on('createRoom', async ({ nickname, gameID }) => {
     nickname = nickname.toString();
@@ -34,10 +34,10 @@ io.on('connection', async (socket) => {
     const hasPlayer = await Room.findOne({ gameID: gameID });
     if (hasPlayer) {
       socket.emit('playexist', { gameID: gameID });
-      console.log('plaYER EXIST');
+      // console.log('plaYER EXIST');
       return;
     } else {
-      console.log('New User');
+      // console.log('New User');
     }
     try {
       let room = new Room();
@@ -52,24 +52,24 @@ io.on('connection', async (socket) => {
       room.gameID = gameID;
       socket.join(gameID.toString());
       room = await room.save();
-      console.log(room);
+      // console.log(room);
       const roomID = room._id.toString();
       socket.join(gameID.toString());
-      console.log(roomID, gameID);
+      // console.log(roomID, gameID);
       io.to(gameID.toString()).emit('createdRoom', room);
       // console.log('S  id  ',socket.id);
       io.to(gameID.toString()).emit('created', { gameID: gameID.toString() });
     } catch (e) {
       console.log(e);
     }
-    console.log(nickname);
+    // console.log(nickname);
   })
   socket.on('joinRand', async ({ nickname }) => {
-    console.log(nickname);
+    // console.log(nickname);
 
   })
   socket.on('joinRandom', async ({ nickname }) => {
-    console.log('Room', nickname);
+    // console.log('Room', nickname);
     full += 1;
     nickname = nickname.toString();
     var gameID = roomno;
@@ -77,9 +77,9 @@ io.on('connection', async (socket) => {
       try {
         let room = await Room.findOne({ gameID: parseInt(gameID) });
         if (room) {
-          console.log('User found:');
+          // console.log('User found:');
         } else {
-          console.log('User not found.');
+          // console.log('User not found.');
           return
         }
         gameID = gameID.toString();
@@ -91,14 +91,14 @@ io.on('connection', async (socket) => {
           }
           socket.join(gameID.toString());
           room.players.push(player);
-          console.log(room);
+          // console.log(room);
           room = await room.save();
           var gameObj;
           var xPlayer;
           var oPlayer;
           try {gameObj = await Room.findOne({ gameID: parseInt(gameID) });
           } catch (e) {
-            console.log(e);
+            // console.log(e);
           }
           if (gameObj) {
             const play = gameObj.players.find(player => player.playerType === 'X');
@@ -113,9 +113,9 @@ io.on('connection', async (socket) => {
           io.to(gameID.toString()).emit('randomplayerJoined', { 'id': socket.id });
           io.to(gameID.toString()).emit('joinFound', { ticID: gameID.toString(), xPlayer, oPlayer });
           socket.emit('joinTic', { ticID: gameID.toString() });
-          console.log('gameID', gameID);
+          // console.log('gameID', gameID);
         }
-        console.log('Joined');
+        // console.log('Joined');
       } catch (e) {
         console.log(e);
       }
@@ -135,17 +135,17 @@ io.on('connection', async (socket) => {
         room.gameID = gameID;
         socket.join(gameID.toString());
         room = await room.save();
-        console.log(room);
+        // console.log(room);
         const roomID = room._id.toString();
         socket.join(gameID.toString());
-        console.log(roomID, gameID);
+        // console.log(roomID, gameID);
         io.to(gameID.toString()).emit('createdRoom', room);
         io.to(gameID.toString()).emit('created', { gameID: gameID.toString() });
       } catch (e) {
         console.log(e);
       }
     }
-    console.log(nickname);
+    // console.log(nickname);
   });
 
   socket.on('joinRoom', async ({ nickname, roomid }) => {
@@ -153,10 +153,10 @@ io.on('connection', async (socket) => {
       let room = await Room.findOne({ gameID: parseInt(roomid) });
 
       if (room) {
-        console.log('User found:', room);
+        // console.log('User found:', room);
         // socket.emit('joinFound',{isplayer:true});
       } else {
-        console.log('User not found.');
+        // console.log('User not found.');
         socket.emit('joinNotFound', { isplayer: true });
         socket.leave(roomid.toString());
         return
@@ -194,7 +194,7 @@ io.on('connection', async (socket) => {
         io.to(roomid.toString()).emit('PlayerJoined', { 'id': socket.id });
         io.to(roomid.toString()).emit('joinFound', { ticID: roomid.toString(), xPlayer, oPlayer });
         socket.emit('joinTic', { ticID: roomid.toString() });
-        console.log("Roomid", roomid.toString());
+        // console.log("Roomid", roomid.toString());
       }
 
     } catch (e) {
@@ -212,9 +212,9 @@ io.on('connection', async (socket) => {
     if (deletedUser) {
       roomno -= 1;
 
-      // console.log('Tic Exit:', deletedUser);
+      // // console.log('Tic Exit:', deletedUser);
     } else {
-      console.log('User not found.');
+      // console.log('User not found.');
     }
     io.to(Ticid.toString()).emit('exitBoth', { Ticid });
     // socket.leave();
@@ -224,29 +224,29 @@ io.on('connection', async (socket) => {
 
   socket.on('sendmessage', function (data) {
     const { sendmessage, roomid } = data;
-    console.log(roomid, sendmessage);
+    // console.log(roomid, sendmessage);
     io.to(roomid.toString()).emit('receiveMessage', sendmessage);
   });
 
   socket.on('clickIndex', async (data) => {
     const { clickIndex, roomid } = data;
-    console.log('Click  ', clickIndex, roomid);
+    // console.log('Click  ', clickIndex, roomid);
     var gameObj;
     try {
       gameObj = await Room.findOne({ 'players.socketID': socket.id });
     } catch (e) {
-      console.log(e);
+      // console.log(e);
     }
     if (gameObj) {
       const play = gameObj.players.find(player => player.socketID === socket.id);
       const player = play.playerType;
-      console.log(player);
+      // console.log(player);
       io.to(roomid.toString()).emit('opponentClick', { clickIndex, player });
     }
   });
 
   socket.on('removedb', async () => {
-    console.log('Remove');
+    // console.log('Remove');
     //   Room.deleteMany({occupancy:2}).then(function(){
     //     console.log("Data deleted"); // Success
     // }).catch(function(error){
@@ -261,7 +261,7 @@ io.on('connection', async (socket) => {
         roomno -= 1;
         // console.log('Deleted User:', deletedUser);
       } else {
-        console.log('User not found.');
+        // console.log('User not found.');
       }
     } catch (error) {
       console.error('Error:', error);
@@ -269,11 +269,11 @@ io.on('connection', async (socket) => {
   })
 
   socket.on('disconnect', async () => {
-    console.log('User disconnected.');
+    // console.log('User disconnected.');
 
     try {
       const hasUser = await Room.findOne({ 'players.socketID': socket.id });
-      console.log("Has User");
+      // console.log("Has User");
       if (hasUser) {
         const playID = hasUser.gameID;
 
@@ -282,7 +282,7 @@ io.on('connection', async (socket) => {
         // socket.leave(playID.toString());
         // console.log('Tic Exit:', deletedUser);
       } else {
-        console.log('User not found.');
+        // console.log('User not found.');
       }
 
 
@@ -295,7 +295,7 @@ io.on('connection', async (socket) => {
         roomno -= 1;
         // console.log('Deleted User:', deletedUser);
       } else {
-        console.log('User not found.');
+        // console.log('User not found.');
       }
     } catch (error) {
       console.error('Error:', error);
@@ -307,13 +307,13 @@ app.use(express.json());
 const DB = 'mongodb+srv://logsak25:abishekdeepak@abishekdb.55wn9ig.mongodb.net/?retryWrites=true&w=majority'
 
 mongoose.connect(DB).then(() => {
-  console.log("connect success");
+  // console.log("connect success");
 }).catch((e) => {
-  console.log("Failed");
+  // console.log("Failed");
 });
 
 server.listen(port, '0.0.0.0', () => {
-  console.log('listening on', port);
+  // console.log('listening on', port);
 });
 
 // mongodb+srv://logsak25:<password>@abishekdb.55wn9ig.mongodb.net/?retryWrites=true&w=majority
